2ee9dc4d168459b447fecc25283529dedcb1baa1
Burchell <jlkaline@me.com>
2021-01-23 19:07:42 -0500

    added list js

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ $(document).ready(function () {
       weatherForecast(searchCity);
   });
 
+  //grab item from local storage
+  var history = JSON.parse(localStorage.getItem("history")) || [];
+//sets history array search to correct length.
+  if (history.length > 0) {
+      weatherFunction(history[history.length - 1]);
+  }// makes a row for each element in history array(searchTerms)
+  for (var i = 0; i < history.length; i++) {
+      createRow(history[i]);
+  }
