ae53b79b44562405442e5ffdd7a962450484b60b
Burchell <jlkaline@me.com>
2021-01-28 08:57:30 -0500

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  //search button on click will get weather info from input text
+  //search button for enter or on mouse click will get weather info from input text
   var input = document.getElementById("search");
   input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
@@ -13,7 +13,7 @@ $(document).ready(function () {
     weatherForecast(searchCity);
     fiveDayForecast(searchCity);
   });
-
+  // local storage
   var saveCity = JSON.parse(localStorage.getItem("history")) || [];
 
   if (saveCity.length > 0) {
@@ -22,7 +22,7 @@ $(document).ready(function () {
   for (var i = 0; i < saveCity.length; i++) {
     createList(saveCity[i]);
   }
-
+  //create list
   function createList(city) {
     var listCity = $("<li>").addClass("list-group-item").text(city);
     $(".history").append(listCity);
@@ -46,7 +46,7 @@ $(document).ready(function () {
   });
 
   var apiKey = "f7cd1c5ba42d617eae2916f00f40439d"
-
+  //Function for current weather
   function weatherForecast(searchCity) {
     //API to get current weather//
     $.ajax({
@@ -61,7 +61,7 @@ $(document).ready(function () {
       }
       $("#current").empty();
 
-      
+
       var cardForecast = $("<div>").addClass("card");
       var cardBodyForecast = $("<div>").addClass("card-body");
       var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
@@ -85,7 +85,7 @@ $(document).ready(function () {
         var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
         var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
 
-
+        //will give the UV a color indicator low, moderate or high
         if (uvResponse < 3) {
           btn.addClass("btn-success");
         } else if (uvResponse > 6) {
@@ -107,7 +107,7 @@ $(document).ready(function () {
       console.log(data);
     });
   }
-  
+
   //API for 5 day forecast
   function fiveDayForecast(searchCity) {
     $.ajax({
@@ -116,28 +116,28 @@ $(document).ready(function () {
     }).then(function (data) {
       console.log(data);
       var results = data.list
-      
-      $("#fiveDay").html("<h4>5-Day Forecast:</h4></br>").append("<div class=\"row\">");
-      
-      for (var i = 0; i < results.length; i++) {
-     
-        if (results[i].dt_txt.indexOf("12:00:00") !== -1) {
-    
-    var columnFive = $("<div>").addClass("col-md-2");
-    var card = $("<div>").addClass("card bg-info text-white");
-    var cardBody = $("<div>").addClass("card-body p-2")
-    var dateCard = $("<h5>").addClass("card-title").text(new Date(results[i].dt_txt).toLocaleDateString());
-    var temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + results[i].main.temp + " °F");
-    var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
-    var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png");
 
-    columnFive.append(card.append(cardBody.append(dateCard, image, humidity, temperature)));
-        
-        $("#fiveDay").append(columnFive);
-             
-            }
+      $("#fiveDay").html("<h4>5-Day Forecast:</h4> </br>").append("<div class=\"row\">");
+
+      for (var i = 0; i < results.length; i++) {
+
+        if (results[i].dt_txt.indexOf("12:00:00") !== -1) {
+
+          var columnFive = $("<div>").addClass("col-md-2");
+          var card = $("<div>").addClass("card bg-info text-white");
+          var cardBody = $("<div>").addClass("card-body p-2")
+          var dateCard = $("<h5>").addClass("card-title").text(new Date(results[i].dt_txt).toLocaleDateString());
+          var temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + results[i].main.temp + " °F");
+          var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+          var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png");
+          //merge and add to page
+          columnFive.append(card.append(cardBody.append(dateCard, image, humidity, temperature)));
+
+          $("#fiveDay").append(columnFive);
+
         }
+      }
     });
-}
+  }
 });
 
